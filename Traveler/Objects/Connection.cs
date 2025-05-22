8ce45150a3bc64705ba58fcd8328f72c66212915
Alexey Puchkov <alexey.pkv@gmail.com>
2025-05-22 13:00:53 +0300

    Starting to work on project

---
+++ Traveler/Objects/Connection.cs
@@ -0,0 +1,41 @@
+﻿namespace Traveler.Objects;
+
+
+public class Connection<I, ND, CD> where I : IComparable<I>
+{
+	#region Public Properties
+	
+	public Node<I, ND, CD>	From	{ get; }
+	public Node<I, ND, CD>	To		{ get; }
+	public CD				Data	{ get; }
+	
+	#endregion
+	
+	
+	#region Constructor
+	
+	public Connection(Node<I, ND, CD> from, Node<I, ND, CD> to, CD data)
+	{
+		if (from.ID.CompareTo(to.ID) < 0)
+		{
+			From = from;
+			To = to;
+		}
+		else
+		{
+			From = to;
+			To = from;
+		}
+		
+		Data = data;
+	}
+	
+	#endregion
+	
+	
+	#region Methods
+	
+	public Node<I, ND, CD> Other(Node<I, ND, CD> other) => From == other ? To : From;
+	
+	#endregion
+}
---
+++ Traveler/Objects/Node.cs
@@ -0,0 +1,24 @@
+﻿namespace Traveler.Objects;
+
+
+public class Node<I, ND, PD>(I id, ND data)
+	where I : IComparable<I>
+{
+	#region Properties
+	
+	public I ID { get; } = id;
+	public ND Data { get; } = data;
+	public Dictionary<I, Connection<I, ND, PD>> Neighbors { get; } = new();
+	
+	#endregion
+	
+	
+	#region Methods
+	
+	public void Connect(Connection<I, ND, PD> connection)
+	{
+		
+	}
+	
+	#endregion
+}
