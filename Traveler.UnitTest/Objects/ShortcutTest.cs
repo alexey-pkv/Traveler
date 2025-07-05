d9c62fa79b3687b7040cadeee924130ccf15ccc2
Alexey Puchkov <alexey.pkv@gmail.com>
2025-07-05 13:11:02 +0300

    Add sanity test to ShortcutTest.cs

--- Traveler.UnitTest/Objects/ShortcutTest.cs
+++ Traveler.UnitTest/Objects/ShortcutTest.cs
@@ -7,6 +7,28 @@ namespace Traveler.UnitTest.Objects;
 [TestFixture]
 public class ShortcutTests
 {
+	#region Sanity
+	
+	[Test]
+	public void Constructor_WithProperties_PropertiesSetCorrectly()
+	{
+		var toNode = new Node<int, int, int>(1, 10);
+		var viaNode = new Node<int, int, int>(2, 20);
+		var shortcut = new Shortcut<int, int, int>
+		{
+			To = toNode,
+			Via = viaNode,
+			Distance = 15.5
+		};
+		
+		Assert.AreEqual(toNode, shortcut.To);
+		Assert.AreEqual(viaNode, shortcut.Via);
+		Assert.AreEqual(15.5, shortcut.Distance);
+		Assert.IsTrue(shortcut.Has);
+	}
+	
+	#endregion
+	
 	#region Has Property Tests
 	
 	[Test]
